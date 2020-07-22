c98c8702e286e6b2fb1e4a4c537ca96057a679ba
Glenn Abastillas <glenn@Glenn.local>
2020-07-21 21:24:07 -0400

    Updated Schema and LabKeySchema in converter.py

--- converter.py
+++ converter.py
@@ -1,95 +1,87 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 from collections import OrderedDict
+from pathlib import Path
 import yaml
 import json
+import pprint
 
-SEP = ' ' * 4
+pp = pprint.PrettyPrinter(indent=4)
 
-def read_txt(filepath):
-    ''' Read in the plain text schema text file and chunk
+class Schema:
 
-    Parameters
-    ----------
-        filepath (str): path to plain text file
+    def __init__(self, path=None):
+        self.path = Path(path) if path is not None else path
 
-    Returns
-    -------
-        List of schema components
-    '''
-    def prev(lines, index):
-        try:
-            return lines[index - 1]
-        except IndexError:
-            return None
+    def _path(self, path=None):
+        return self.path if path is None else Path(path)
 
-    def post(lines, index):
-        try:
-            return lines[index + 1]
-        except IndexError:
-            return None
+    def load(self, path=None):
+        ''' Return a dict from a yaml-structured schema file '''
+        path = self._path(path)
 
-    def surroundings(lines, index):
-        return prev(lines, index), post(lines, index)
+        if path:
+            with open(path) as data:
+                return yaml.load(data, yaml.Loader)
 
-    schema = OrderedDict()
+        raise ValueError("Path to file to convert to schema not defined")
 
-    with open(path) as fin:
-        lines = fin.readlines()
+class LabKeySchema(Schema):
 
-        h = 0
-        for i, line in enumerate(lines):
-            if indents(line) == 0 and i > 0:
-                header, content = parse(lines, h, i)
-                schema[header] = content
-                h = i + 1
+    def __init__(self, path=None):
+        super().__init__(path)
 
-def indents(line):
-    return line.count(SEP)
+    def convert(self):
+        ''' Convert a yaml-like schema to LabKey's json format '''
+        data = self.load()
 
-def parse(lines, start, finish, level=0):
-    ''' Read a line and determine its schema role
+        tables = []
 
-    Parameters
-    ----------
-        lines (list): lines from plain text file
-        start (int): first index
-        finish (int): second index
-    '''
-    if not isinstance(lines, list):
-        return lines
+        for table_, fields_ in data.items():
+            fields = self.parse(fields_)
+            table = {"table" : table_, "fields" : fields}
+            tables.append(table)
 
-    level = lines[0].replace(SEP, '')
+        groupings = [self._set_group("table"), self._set_group("field")]
+        return {"pathology": {"groupings": groupings, "tables": tables}}
 
-    if level == 0:
-        content = parse(lines[1:], start, finish, level + 1)
-    elif level >= 1:
-        content =
+    def parse(self, fields):
+        ''' Return a list of field values converted to LabKey's format '''
+        parsed = []
+
+        for field, values in fields.items():
+            field_ = self._set_field(field, values)
+            # pp.pprint(field_)
+            parsed.append(field_)
+
+        return parsed
+
+    def _set_field(self, name, values):
+        class_, type_, *values_ = values
+        return {'field' : name,
+                'datatype' : 'date' if type_.lower() == 'date' else 'string',
+                'closedClass' : True if class_.lower().startswith('c') else False,
+                'multiValue': False,
+                'diseaseProperties' : [
+                     {
+                         'diseaseGroup': ["*"],
+                         'values': values_
+                     }
+               ]}
+
+    def _set_group(self, level, order="alpha", orientation="horizontal"):
+        return dict(level=level, order=order, orientation=orientation)
+
+    def save(self, path=None):
+        ''' Convert and save a yaml-like schema to LabKey's format '''
+        path = self._path(path)
+        save_path = path.parent / f'{path.stem}.json'
+
+        with open(save_path, 'w') as pout:
+            json.dump(self.convert(), pout)
 
 
-    return {level : content}
-
-    section, field, values = [[]] * 3
-    h = 0
-
-    for i, line in enumerate(lines[start:finish]):
-        if indents(line) == 0:
-
-def make_schema(filepath):
-    ''' Return a schema for upload into LabKey
-
-    Parameters
-    ----------
-        filepath (str): path to plain text file
-    '''
-    raw = read_txt(filepath)
-
-if __name__ == "__main__":
-    parse(['    section name',
-           '        field',
-           '        class',
-           '        type',
-           '        values',
-           '        values',
-           '            secondary values',
-           ], 0, 7)
+if __name__=="__main__":
+    print(LabKeySchema('resources/schema.txt').save())
+    # a, b, *c = Schema('resources/schema.txt').load()['Circumferential Resection Margin (CRM)']['Measurement Unit']
+    # print(a, b, c)
