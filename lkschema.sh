079e13802fd32a9560f0d2d3ef42245c938425e5
Glenn Abastillas <glenn@Glenn.local>
2020-08-07 19:09:47 -0500

    Added lkschema.sh

---
+++ lkschema.sh
@@ -0,0 +1,11 @@
+ERROR="No filename specified. Please specify a filename to convert (e.g., resource/schema.txt)."
+INVALID_PATH="Specified path does not exist."
+SUCCESS="Converting $1 to "
+
+if [ "$1" = "" ]; then
+    echo $ERROR;
+elif [ ! -d "$1" ]; then
+    echo "$INVALID_PATH"
+fi
+
+# python -m main -f %1
