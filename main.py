65b36e7297cb38a597fbae7b9a87fcef380304e5
gabastil <glenn.abastillas@gmail.com>
2021-05-11 08:44:50 -0400

    Add -o / --output flag to main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from optparse import OptionParser
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-f', '--filename', dest='filename', default='resources/schema.txt')
+    parser.add_option('-o', '--output', dest='output')
 
     (options, args) = parser.parse_args()
 
     print(f'Converting schema at {options.filename}')
 
     schema = LabKeySchema(options.filename)
-    schema.save()
+    schema.save(options.output)
