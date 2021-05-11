46398d43a589e13375eeab5cfa9e6d15e799b0db
Glenn <glenn.abastillas@gmail.com>
2021-05-11 14:18:51 +0100

    Quickly refactor lkschema.sh script

--- lkschema.sh
+++ lkschema.sh
@@ -2,12 +2,20 @@
 
 ERROR="No filename specified. Please specify a filename to convert (e.g., resource/schema.txt)."
 INVALID_PATH="Specified path does not exist."
-SUCCESS="Converting $1 to "
 
-if [ "$1" = "" ]; then
-    echo $ERROR;
+if [ -z $1 ]; then
+    echo $ERROR
 elif [ ! -d "$1" ]; then
-    echo "$INVALID_PATH"
+    echo $INVALID_PATH
 fi
 
-# python -m main -f %1
+DIRNAME=$(dirname $1)
+BASENAME=$(basename $1)
+OUTPUT=$BASENAME.json
+
+if [ -n $2 ]; then
+    OUTPUT=$2
+fi
+
+echo Converting $1 to $OUTPUT
+# python -m main -f %1 -o %2
