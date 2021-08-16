b17f4a9829dfe338d19556832237b5fb69c29b9a
gabastil <glenn.abastillas@gmail.com>
2021-08-16 17:31:21 -0400

    Refactor script and move source code to src

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from converter import LabKeySchema
+from src.converter import LabKeySchema
 from optparse import OptionParser
+from pathlib import Path
+from box import Box
+import os
+
+
+def main(options):
+    options.filename = Path(os.path.dirname(__file__),
+                            options.source_dir,
+                            options.filename)
+
+    schema = LabKeySchema(options.filename, options=options)
+    schema.save(options.output)
+
+
+def load_configuration(fp='config.yml', basedir='config'):
+    with open(os.path.join(basedir, fp)) as fin:
+        return Box.from_yaml(fin)
+
+
+def get_option_parser(fp='config.yml', basedir='config'):
+    config_yml = load_configuration(fp, basedir)
+
+    parser = OptionParser('CLI tool to generate new schemas')
+    for option in config_yml.cli:
+        parser.add_option(*option.flags, **option.kwargs)
+
+    return parser
+
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option('-f', '--filename', dest='filename', default='resources/schema.txt')
-    parser.add_option('-o', '--output', dest='output')
-
-    (options, args) = parser.parse_args()
+    parser = get_option_parser()
+    options, *_ = parser.parse_args()
 
     print(f'Converting schema at {options.filename}')
-
-    schema = LabKeySchema(options.filename)
-    schema.save(options.output)
+    main(options)
