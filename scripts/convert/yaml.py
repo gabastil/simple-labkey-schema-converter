db7f178fa1bf087b01c94059747cd0e07db39841
gabastil <glenn.abastillas@gmail.com>
2022-03-17 07:21:05 -0400

    Incorporate pathlib

--- scripts/convert/yaml.py
+++ scripts/convert/yaml.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 from src.converter import LabKeySchema
 from optparse import OptionParser
+from pathlib import Path
 import os
 
 CONFIG = {
@@ -27,24 +28,26 @@ CONFIG = {
 def main(options):
     make_filenames(options)
     print(f"{f' YAML CONVERTER ':-^80}")
-    print(f"Filename: {options.filename}\nOutput dir: {options.output_dir}")
-
+    print(f"Filename: {options.filename.name}",
+          f"Source dir: {options.data_dir}"
+          f"Output dir: {options.output_dir}"
+          sep="\n")
     schema = LabKeySchema(options.filename, options=options)
-    schema.save(options.output)
+    schema.save(options.output_filename)
+    print(f"{f' CONVERSION COMPLETE ':-^80}")
 
 
 def make_filenames(options):
-    options.filename = os.path.join(options.data_dir, options.file)
-    output_filename = f"{os.path.basename(options.file).split('.')[0]}.json"
-    options.output_filename = os.path.join(options.output_dir, output_filename)
+    options.filename = options.data_dir / options.file
+    output_filename = f"{'.'.join(options.file.split('.')[:-1])}.json"
+    options.output_filename = options.output_dir / output_filename
 
 
 if __name__ == "__main__":
-    # parser = get_option_parser()
     parser = OptionParser("CLI tool to convert YAML schemas to JSON")
     parser.add_option("--file", "-f")
-    parser.add_option("--data_dir", default="data/dataset/reports")
-    parser.add_option("--output_dir", default="data/output/converted")
+    parser.add_option("--data_dir", default=Path("data/dataset/yml"))
+    parser.add_option("--output_dir", default=Path("data/output/json"))
     parser.add_option("--disease_group", default="*")
     parser.add_option("--docDate", default=None)
     parser.add_option("--docName", default=None)
@@ -52,5 +55,6 @@ if __name__ == "__main__":
     parser.add_option("--engine_version", default=CONFIG['engine_version'])
     parser.add_option("--process_date", default=None)
     parser.add_option("--reference_id", default=None)
-    parser.add_option("--control_info_fields", default=CONFIG['control_info'])
+    parser.add_option("--control_info_fields",
+                      default=CONFIG['control_info'])
     main(parser.parse_args()[0])
